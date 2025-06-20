cc5e3b79c23f277029ee7ead5ce5d51f7ae7470f
Dingfeng Wu <799390224@qq.com>
2025-06-20 13:12:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.1.0',
     description='miProbe Toolkit: Microbial peptide embedding and AI-ready interface',
     author='Dingfeng Wu',
-    author_email='example@example.com',
-    packages=find_packages(),
+    author_email='dfw_bioinfo@126.com',
+    packages=find_packages(include=["miprobe", "miprobe.*"]),
     install_requires=['requests', 'numpy', 'torch'],
     python_requires='>=3.8',
 )
